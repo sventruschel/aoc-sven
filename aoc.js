f7a06f7d7488a5aa2d76923ef0d74b767e4bcab5
sventruschel <67267206+sventruschel@users.noreply.github.com>
2021-12-01 09:29:23 +0100

    Add files via upload

---
+++ aoc.js
@@ -0,0 +1,31 @@
+import { readFileSync } from "fs";
+const lines = readFileSync("input.txt", { encoding: "utf-8" })
+  .split("\n")
+  .filter((x) => x)
+  .map((x) => parseInt(x));
+
+function countBiggerThanPrevious1(array) {
+  var count = 0;
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] > array[i - 1]) {
+      count++;
+    }
+  }
+  return count;
+}
+
+function countBiggerThanPrevious2(array) {
+  var count = 0;
+  for (var i = 0; i < array.length; i++) {
+    if (
+      array[i] + array[i + 1] + array[i + 2] <
+      array[i + 1] + array[i + 2] + array[i + 3]
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countBiggerThanPrevious1(lines));
+console.log(countBiggerThanPrevious2(lines));
